2c3fcfae9ddce667c6cbdb3de3b08e738aa8697b
struggle-Strug <strugstruggle0430@gmail.com>
2024-09-11 01:34:06 +0900

    create: initial commit

---
+++ config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: `${process.env.HOST}`,
+    user: `${process.env.USERNAME}`,
+    password: `${process.env.PASSWORD}`,
+    database: `${process.env.DATABASE}`,
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors("*"));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
